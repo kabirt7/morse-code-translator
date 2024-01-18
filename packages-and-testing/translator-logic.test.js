4c42596f0cb1757ce11add49240008a472464b2e
kabirt7 <kabir.t7@hotmail.com>
2024-01-18 12:40:37 +1100

    more tests

--- packages-and-testing/translator-logic.test.js
+++ packages-and-testing/translator-logic.test.js
@@ -13,8 +13,19 @@ const mockDomML = {
 describe("Tests translator function", () => {
   it("should return correct morse output", () => {
     expect(translate("hiya", mockDomLM)).toBe(".... .. -.-- .- ");
+    expect(
+      translate("the quick brown fox jumped over the lazy dog", mockDomLM)
+    ).toBe(
+      "- .... . / --.- ..- .. -.-. -.- / -... .-. --- .-- -. / ..-. --- -..- / .--- ..- -- .--. . -.. / --- ...- . .-. / - .... . / .-.. .- --.. -.-- / -.. --- --. "
+    );
   });
   it("should return correct latin script output", () => {
+    expect(
+      translate(
+        "- .... . / --.- ..- .. -.-. -.- / -... .-. --- .-- -. / ..-. --- -..- / .--- ..- -- .--. . -.. / --- ...- . .-. / - .... . / .-.. .- --.. -.-- / -.. --- --.",
+        mockDomML
+      )
+    ).toBe("THE QUICK BROWN FOX JUMPED OVER THE LAZY DOG");
     expect(translate(".... .. -.-- .- ", mockDomML)).toBe("HIYA");
   });
 
